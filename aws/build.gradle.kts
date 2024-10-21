441bcfd50c07f66605b03bd1e2ea90f2a3d410a3
weichaoa <118242036+weichaoa@users.noreply.github.com>
2024-10-21 09:24:15 +0000

    android automotive first application
    android automotive first application.

---
+++ aws/build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    val kotlin_version by extra("1.8.22") // 统一 Kotlin 版本
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
